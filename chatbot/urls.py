4a974d95fe14af3389e1f8ebbfb26641a4a7f572
piyushsoni813 <piyushsoni0286@gmail.com>
2023-10-25 03:18:52 +0530

    imtial commit

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chatbot, name='chatbot'),
-    path('login', views.login, name='login'),
+    path('', views.login, name='login'),
+    path('chatbot', views.chatbot, name='chatbot'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
 ]
